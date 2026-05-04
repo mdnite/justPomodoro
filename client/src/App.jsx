155893950b086b71ecadec7e6b284ee1f57a1767
mdnite <tanminhnguyen2004@gmail.com>
2026-05-04 21:20:28 +0700

    Bug fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import Timer from './components/Timer/Timer';
+import { useAuth } from './context/AuthContext';
 import './App.css';
 
 function App() {
+  const { logout } = useAuth();
+
   return (
     <main className="app">
       <h1 className="app__title">Just Pomodoro</h1>
       <Timer />
+      <button className="app__logout" onClick={logout}>Logout</button>
     </main>
   );
 }
